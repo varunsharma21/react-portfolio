200076afb6e3c45fff53b1b7e4c2643b4b65f1fe
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2022-02-14 21:08:04 +0530

    Add files via upload

--- src/Components/topbar/Topbar.js
+++ src/Components/topbar/Topbar.js
@@ -1,11 +1,31 @@
-import './Topbar.scss'
+import './Topbar.scss';
+import {Person, Mail} from "@material-ui/icons";
 
-function topbar() {
+function topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='topbar'>
-        
+    <div className={'topbar ' + (menuOpen && "active")}>
+        <div className='wrapper'>
+            <div className='left'>
+                <a href='#intro' className='logo'> Varun.</a>
+                <div className='itemContainer'>
+                    <Person className='icon' />
+                    <span>+91 7042929823</span>
+                </div>
+                <div className='itemContainer'>
+                    <Mail className='icon' />
+                    <span>vsvarun.sharma2112@gmail.com</span>
+                </div>
+            </div>
+            <div className='right'>
+                <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+                  <span className='line1'></span>
+                  <span className='line2'></span>
+                  <span className='line3'></span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default topbar
+export default topbar;
