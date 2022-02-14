c2a7fabe0e82218f523793db97e4906417dd5a70
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2022-02-14 21:06:30 +0530

    Add files via upload

---
+++ src/Components/menu/Menu.js
@@ -0,0 +1,27 @@
+import "./Menu.scss";
+
+function Menu() {
+  return (
+    <div>
+        <ul>
+            <li>
+                <a href='#intro'>Home</a> 
+            </li>
+            <li>
+                <a href='#portfolio'>Portfolio</a> 
+            </li>
+            <li>
+                <a href='#works'>Work</a> 
+            </li>
+            <li>
+                <a href='#testimonials'>Testimonials</a> 
+            </li>
+            <li>
+                <a href='#contact'>Contact</a> 
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Menu;
