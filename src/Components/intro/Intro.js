7d4e0c8531344cda174bd0de919a75d74bff168f
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2022-02-15 21:56:41 +0530

    Add files via upload

--- src/Components/intro/Intro.js
+++ src/Components/intro/Intro.js
@@ -3,9 +3,14 @@ import './Intro.scss'
 function intro() {
   return (
     <div className='intro' id='intro'>
-        
+        <div className='left'>
+          <div className='imageContainer'>
+            <img src='' alt='' />
+          </div>
+        </div>
+        <div className='right'></div>
     </div>
   )
 }
 
-export default intro
+export default intro;
